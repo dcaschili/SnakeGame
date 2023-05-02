6f5462ffcfb2617f6c48bdd2d99d6618f78f6d20
dcaschili <daniele.caschili@outlook.com>
2023-05-02 13:51:36 +0200

    Added main menu page.

---
+++ SnakeGame/Source/SnakeGame/Include/SnakeGamePlayerController.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "GameFramework/PlayerController.h"
+
+#include "SnakeGamePlayerController.generated.h"
+
+UCLASS()
+class SNAKEGAME_API ASnakeGamePlayerController : public APlayerController
+{
+    GENERATED_BODY()
+
+};
