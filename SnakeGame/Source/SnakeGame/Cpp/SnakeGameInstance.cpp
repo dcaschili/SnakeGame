3b51d520b5c0af05d4593341d3cd66b28f095aee
dcaschili <daniele.caschili@outlook.com>
2023-09-12 10:38:17 +0200

    Enabled slate global invalidation.

--- SnakeGame/Source/SnakeGame/Cpp/SnakeGameInstance.cpp
+++ SnakeGame/Source/SnakeGame/Cpp/SnakeGameInstance.cpp
@@ -1,1 +1,17 @@
 #include "SnakeGameInstance.h"
+
+#include "SnakeLog.h"
+#include "HAL/IConsoleManager.h"
+
+void USnakeGameInstance::Init()
+{
+	Super::Init();
+
+	IConsoleVariable* CVarGlobalInvalidation = IConsoleManager::Get().FindConsoleVariable(TEXT("Slate.EnableGlobalInvalidation"));
+	if (CVarGlobalInvalidation)
+	{
+		GDTUI_SHORT_LOG(SnakeLogCategoryUI, Log, TEXT("Enabling slate global invalidation!"));
+		CVarGlobalInvalidation->Set(1);
+	}
+
+}
